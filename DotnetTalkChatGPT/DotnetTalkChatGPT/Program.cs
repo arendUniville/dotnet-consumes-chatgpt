9277b44e529267364d33182207bfb839448d7eff
Arend <matheus.arend@univille.br>
2024-05-18 11:47:58 -0300

    Configurando serilog

--- DotnetTalkChatGPT/DotnetTalkChatGPT/Program.cs
+++ DotnetTalkChatGPT/DotnetTalkChatGPT/Program.cs
@@ -1,23 +1,25 @@
 using DotnetTalkChatGPT.Extensions;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.AddSerilog(builder.Configuration, "Dotnet 8 Talk With ChatGPT");
 
 //Carregando infos de configuração do chatgpt
 builder.AddChatGpt(builder.Configuration);
 
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
